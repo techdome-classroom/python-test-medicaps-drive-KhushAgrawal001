9183373f7bd81c2abea2c16ccd554ad577363527
Khush Agrawal <Khushagrawal2404@gmail.com>
2024-04-25 14:04:10 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -4,4 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        prev_value = 0
+
+        for char in s:
+            current_value = roman_values[char]
+            if current_value > prev_value:
+                result += current_value - 2 * prev_value
+            else:
+                result += current_value
+            prev_value = current_value
+
+        return result
+
+# Test cases
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.romanToInt("III"))      # Output: 3
+    print(solution.romanToInt("LVIII"))    # Output: 58
+    print(solution.romanToInt("MCMXCIV"))  # Output: 1994
