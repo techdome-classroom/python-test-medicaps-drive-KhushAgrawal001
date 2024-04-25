e1b39a02781c9e11141407043cd358ddd84b3ab4
Khush Agrawal <Khushagrawal2404@gmail.com>
2024-04-25 14:10:27 +0530

    Final program2.py

--- program2.py
+++ program2.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution:
     def romanToInt(self, s):
         """
         :type s: str
@@ -8,9 +8,9 @@ class Solution(object):
         result = 0
         prev_value = 0
 
-        for char in s:
-            current_value = roman_values[char]
-            if current_value > prev_value:
+        for i in range(len(s)):
+            current_value = roman_values[s[i]]
+            if i > 0 and current_value > prev_value:
                 result += current_value - 2 * prev_value
             else:
                 result += current_value
@@ -24,3 +24,7 @@ if __name__ == "__main__":
     print(solution.romanToInt("III"))      # Output: 3
     print(solution.romanToInt("LVIII"))    # Output: 58
     print(solution.romanToInt("MCMXCIV"))  # Output: 1994
+    print(solution.romanToInt("IV"))       # Output: 4
+    print(solution.romanToInt("IX"))       # Output: 9
+    print(solution.romanToInt("MMMCMXCIX")) # Output: 3999
+    print(solution.romanToInt(""))         # Output: 0
